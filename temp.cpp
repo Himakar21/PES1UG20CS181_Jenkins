24fda7226ecf081d88badaadef5622195c8a1c08
Himakar <95559241+Himakar21@users.noreply.github.com>
2023-02-15 19:51:01 +0530

    Create temp.cpp

---
+++ temp.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World I am J.Himakar . My SRN is PES1UG20CS181 \n";
+  cout << "I am in Task5 pipelines .\n";
+  return 0;
+}
